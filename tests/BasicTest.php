ee7e7c6aa7bc6d6221a067634a12524ee716e28a
Lars Moelleken <lmoelleken@menadwork.com>
2015-07-17 18:19:32 +0200

    [+]: added one more test-mail

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -40,62 +40,6 @@ EOT;
     self::assertEquals($expected_output, $output);
   }
 
-  public function testHtmlToText1()
-  {
-    $html = UTF8::file_get_contents(__DIR__ . '/test1Html.html');
-
-    $html2text = new Html2Text($html, false, array('directConvert' => true));
-
-    $text = $html2text->getText();
-
-    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test1Html.txt'), $text);
-  }
-
-  public function testHtmlToText2()
-  {
-    $html = UTF8::file_get_contents(__DIR__ . '/test2Html.html');
-
-    $html2text = new Html2Text($html, false, array('directConvert' => true));
-
-    $text = $html2text->getText();
-
-    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test2Html.txt'), $text);
-  }
-
-  public function testHtmlToText3()
-  {
-    $html = UTF8::file_get_contents(__DIR__ . '/test3Html.html');
-
-    $html2text = new Html2Text($html, false, array('directConvert' => true));
-
-    $text = $html2text->getText();
-
-    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test3Html.txt'), $text);
-
-  }
-
-  public function testHtmlToText4()
-  {
-    $html = UTF8::file_get_contents(__DIR__ . '/test4Html.html');
-
-    $html2text = new Html2Text($html, false, array('directConvert' => true));
-
-    $text = $html2text->getText();
-
-    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test4Html.txt'), $text);
-  }
-
-  public function testHtmlToText5()
-  {
-    $html = UTF8::file_get_contents(__DIR__ . '/test5Html.html');
-
-    $html2text = new Html2Text($html, false, array('directConvert' => true));
-
-    $text = $html2text->getText();
-
-    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test5Html.txt'), $text);
-  }
-
   /**
    * testDoLinksNone
    */
---
+++ tests/MailTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace voku\Html2Text\tests;
+
+use \voku\helper\UTF8;
+use \voku\Html2Text\Html2Text;
+
+/**
+ * Class BasicTest
+ *
+ * @package Html2Text
+ */
+class MailTest extends \PHPUnit_Framework_TestCase
+{
+
+  public function testHtmlToText1()
+  {
+    $html = UTF8::file_get_contents(__DIR__ . '/test1Html.html');
+
+    $html2text = new Html2Text($html, false, array('directConvert' => true));
+
+    $text = $html2text->getText();
+
+    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test1Html.txt'), $text);
+  }
+
+  public function testHtmlToText2()
+  {
+    $html = UTF8::file_get_contents(__DIR__ . '/test2Html.html');
+
+    $html2text = new Html2Text($html, false, array('directConvert' => true));
+
+    $text = $html2text->getText();
+
+    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test2Html.txt'), $text);
+  }
+
+  public function testHtmlToText3()
+  {
+    $html = UTF8::file_get_contents(__DIR__ . '/test3Html.html');
+
+    $html2text = new Html2Text($html, false, array('directConvert' => true));
+
+    $text = $html2text->getText();
+
+    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test3Html.txt'), $text);
+
+  }
+
+  public function testHtmlToText4()
+  {
+    $html = UTF8::file_get_contents(__DIR__ . '/test4Html.html');
+
+    $html2text = new Html2Text($html, false, array('directConvert' => true));
+
+    $text = $html2text->getText();
+
+    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test4Html.txt'), $text);
+  }
+
+  public function testHtmlToText5()
+  {
+    $html = UTF8::file_get_contents(__DIR__ . '/test5Html.html');
+
+    $html2text = new Html2Text($html, false, array('directConvert' => true));
+
+    $text = $html2text->getText();
+
+    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test5Html.txt'), $text);
+  }
+
+  public function testHtmlToText6()
+  {
+    $html = UTF8::file_get_contents(__DIR__ . '/test6Html.html');
+
+    $html2text = new Html2Text($html, false, array('directConvert' => true));
+
+    $text = $html2text->getText();
+
+    self::assertEquals(UTF8::file_get_contents(__DIR__ . '/test6Html.txt'), $text);
+  }
+}
