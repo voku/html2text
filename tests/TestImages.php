fb651f075e5143cb949a7cd591fc384cdc3d2ddb
Lars Moelleken <lmoelleken@menadwork.com>
2015-10-11 03:25:53 +0200

    [+]: added some more tests for images | thx @andrewnicols
    -> https://github.com/andrewnicols/html2text/commit/f0b207d0c2d47dbb30aac5dd7335b74f40a5a404

--- tests/TestImages.php
+++ tests/TestImages.php
@@ -21,4 +21,40 @@ class ImageTest extends \PHPUnit_Framework_TestCase
     $this->assertEquals('image: "This is our cool logo"
 image: \'This is our cool logo\'', $html->getText());
   }
+
+  public function testImageDataProvider() {
+    return array(
+        'Without alt tag' => array(
+            'html' => '<img src="http://example.com/example.jpg">',
+            'expected'  => '',
+        ),
+        'Without alt tag, wrapped in text' => array(
+            'html' => 'xx<img src="http://example.com/example.jpg">xx',
+            'expected'  => 'xxxx',
+        ),
+        'With alt tag' => array(
+            'html' => '<img src="http://example.com/example.jpg" alt="An example image">',
+            'expected'  => 'image: "An example image"',
+        ),
+        'With alt, and title tags' => array(
+            'html' => '<img src="http://example.com/example.jpg" alt="An example image" title="Should be ignored">',
+            'expected'  => 'image: "An example image"',
+        ),
+        'With alt tag, wrapped in text' => array(
+            'html' => 'xx <img src="http://example.com/example.jpg" alt="An example image"> xx',
+            'expected'  => 'xx image: "An example image" xx',
+        ),
+    );
+  }
+
+  /**
+   * @dataProvider testImageDataProvider
+   */
+  public function testImages($html, $expected)
+  {
+    $html2text = new Html2Text($html);
+    $output = $html2text->getText();
+
+    $this->assertEquals($expected, $output);
+  }
 }
